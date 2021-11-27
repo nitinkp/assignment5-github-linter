c63b7afb9b7d9757d9cec8e2163253d014f1dbf3
Nithin <pnithinkumar99@gmail.com>
2021-11-27 01:13:21 -0600

    Second commit

--- main.py
+++ main.py
@@ -5,29 +5,50 @@ import re
 from git import Repo
 import stat
 
+folder_name = 'files_from_git'
 
-# print("gvwefvge")
-# def rmtree(top):
-#     for root, dirs, files in os.walk(top, topdown=False):
-#         for name in files:
-#             filename = os.path.join(root, name)
+# def folderremover(foldername):
+#     for root, dir, files in os.walk(foldername, topdown=False):
+#         for file in files:
+#             filename = os.path.join(root, file)
 #             os.chmod(filename, stat.S_IWUSR)
 #             os.remove(filename)
-#         for name in dirs:
-#             os.rmdir(os.path.join(root, name))
-#     os.rmdir(top)
-#
-# rmtree('files_from_git')
+#         for file in dir:
+#             os.rmdir(os.path.join(root, file))
+#     os.rmdir(foldername)
+
+# folderremover(folder_name)
+
 
 site_name = input("enter public github site, remember to begin with https:// : ")
 site = requests.get(site_name)
-Repo.clone_from(site_name + '.git', 'files_from_git')
+Repo.clone_from(site_name + '.git', folder_name)
 print(site.status_code)
+
+extension = input("enter the file extension: whether .py or .js or .go or .rb")
+ex_allowed = ['.py','.js','.go','.rb']
+if extension in ex_allowed:
+    print("allowed extension")
+else:
+    print("extension not in allowed list of extension")
+
+language = input("enter programming language : ")
+allowed = ['python', 'ruby', 'javascript', 'go']
+if language in allowed:
+    print("allowed language")
+else:
+    print("language not in allowed list of languages")
+
+out1 = input("Enter the text file to write output 1")
+out2 = input("Enter the text file to write output 2") #
+
+
 py_paths = []
 go_paths = []
 js_paths = []
 ruby_paths = []
-for root, dirs, files in os.walk(r'files_from_git'):
+
+for root, dir, files in os.walk(r'files_from_git'):
     for file in files:
         if file.endswith('.py'):
             py_paths.append(os.path.join(root, file))
@@ -39,10 +60,10 @@ for root, dirs, files in os.walk(r'files_from_git'):
             ruby_paths.append(os.path.join(root, file))
 # out=site.text
 i = 0
-print(py_paths)
+# print(py_paths)
 
 py_codes = []
-while (i < len(py_paths)):
+while i < len(py_paths):
     with open(py_paths[i]) as op:
         try:
             contents = op.read()
@@ -52,9 +73,8 @@ while (i < len(py_paths)):
             pass
     i = i + 1
 
-
 ruby_codes = []
-while (i < len(ruby_paths)):
+while i < len(ruby_paths):
     with open(ruby_paths[i]) as rb:
         try:
             conts = rb.read()
@@ -63,28 +83,30 @@ while (i < len(ruby_paths)):
         except:
             pass
     i = i + 1
+
 js_codes = []
-print(js_paths)
-while (i<len(js_paths)):
+# print(js_paths)
+while i < len(js_paths):
     with open(js_paths[i]) as f:
         try:
             cont = f.read()
             js_codes.append(cont)
         except:
             pass
-    i=i+1
+    i = i + 1
+
 go_codes = []
-while (i < len(go_paths)):
+while i < len(go_paths):
     with open(go_paths[i]) as go:
         try:
             conts = go.read()
-
             go_codes.append(conts)
         except:
             pass
     i = i + 1
 
-def rmtree(top):
+
+def folderremover(top):
     for root, dirs, files in os.walk(top, topdown=False):
         for name in files:
             filename = os.path.join(root, name)
@@ -95,108 +117,126 @@ def rmtree(top):
     os.rmdir(top)
 
 
-rmtree('files_from_git')
+folderremover(folder_name)
 
 Language.build_library('build/my-languages.so',
                        ['tree-sitter-go', 'tree-sitter-javascript', 'tree-sitter-python', 'tree-sitter-ruby'])
+
+# python
 py_parser = Parser()
 PYTHON_LANGUAGE = Language('build/my-languages.so', 'python')
 py_parser.set_language(PYTHON_LANGUAGE)
 
+# ruby
 ruby_parser = Parser()
 RUBY_LANGUAGE = Language('build/my-languages.so', 'ruby')
 ruby_parser.set_language(RUBY_LANGUAGE)
 
+# javascript
 js_parser = Parser()
 JS_LANGUAGE = Language('build/my-languages.so', 'javascript')
 js_parser.set_language(JS_LANGUAGE)
 
+# go
 go_parser = Parser()
 GO_LANGUAGE = Language('build/my-languages.so', 'go')
 go_parser.set_language(GO_LANGUAGE)
 
 
-def parsing(source, parser):
-    li = []
+def parser(source, parser1):
+    list = []
 
-    def parseNode(root):
-        if (len(root.children) == 0):
+    def node_parser(root1):
+        if len(root1.children) == 0:
             return
         else:
-            for i in root.children:
-                if (i.type == 'identifier'):
-                    li.append(i)
+            for j in root1.children:
+                if j.type == 'identifier':
+                    list.append(j)
 
-                parseNode(i)
+                node_parser(j)
 
-    tree = parser.parse(bytes(source, "utf8"))
+    tree = parser1.parse(bytes(source, "utf8"))
     root_node = tree.root_node
-    parseNode(root_node)
-    return li
+    node_parser(root_node)
+    return list
 
-py_ids=[]
-for i in range(len(py_codes)):
-    print("output for", py_paths[i])
-    li = parsing(py_codes[i], py_parser)
-    code = py_codes[i].split('\n')
-    ids = []
-    for e in li:
-        row_no = e.start_point[0]
-        col_no = e.start_point[1]
-        print(code[row_no][col_no:e.end_point[1]], "Row No", row_no, " Column No", col_no)
-        ids.append(code[row_no][col_no:e.end_point[1]])
-    py_ids.append(ids)
-    print("-----------------------------------------------------------------------------------")
-    print('\n')
 
-for i in range(len(ruby_codes)):
-    print("output for", ruby_paths[i])
-    li = parsing(ruby_codes[i], ruby_parser)
-    code = ruby_codes[i].split('\n')
-    ids = []
-    for e in li:
-        row_no = e.start_point[0]
-        col_no = e.start_point[1]
-        print(code[row_no][col_no:e.end_point[1]], "Row No", row_no, " Column No", col_no)
-    print("-----------------------------------------------------------------------------------")
-    print('\n')
+py_ids = []
 
-for i in range(len(js_codes)):
-    print("output for", js_paths[i])
-    li = parsing(js_codes[i], js_parser)
-    code = js_codes[i].split('\n')
-    ids = []
-    for e in li:
-        row_no = e.start_point[0]
-        col_no = e.start_point[1]
-        print(code[row_no][col_no:e.end_point[1]], "Row No", row_no, " Column No", col_no)
-    print("-----------------------------------------------------------------------------------")
-    print('\n')
 
-for i in range(len(go_codes)):
-    print("output for", go_paths[i])
-    li = parsing(go_codes[i], go_parser)
-    code = go_codes[i].split('\n')
-    ids = []
-    for e in li:
-        row_no = e.start_point[0]
-        col_no = e.start_point[1]
-        print(code[row_no][col_no:e.end_point[1]], "Row No", row_no, " Column No", col_no)
-    print("-----------------------------------------------------------------------------------")
-    print('\n')
+def py_printer():
+    for i in range(len(py_codes)):
+        print("output for", py_paths[i])
+        py_list = parser(py_codes[i], py_parser)
+        code = py_codes[i].split('\n')
+        ids = []
+        for e in py_list:
+            row = e.start_point[0]
+            col = e.start_point[1]
+            print(code[row][col:e.end_point[1]], "Row Num", row, " Column Num", col)
+            ids.append(code[row][col:e.end_point[1]])
+        py_ids.append(ids)
+        print("**********************************************")
+        print('\n')
+
+
+def ruby_printer():
+    for i in range(len(ruby_codes)):
+        print("output for", ruby_paths[i])
+        ruby_list = parser(ruby_codes[i], ruby_parser)
+        code = ruby_codes[i].split('\n')
+        ids = []
+        for e in ruby_list:
+            row = e.start_point[0]
+            col = e.start_point[1]
+            print(code[row][col:e.end_point[1]], "Row Num", row, " Column Num", col)
+        print("**********************************************")
+        print('\n')
+
+
+def js_printer():
+    for i in range(len(js_codes)):
+        print("output for", js_paths[i])
+        ruby_list = parser(js_codes[i], js_parser)
+        code = js_codes[i].split('\n')
+        ids = []
+        for e in ruby_list:
+            row = e.start_point[0]
+            col = e.start_point[1]
+            print(code[row][col:e.end_point[1]], "Row Num", row, " Column Num", col)
+        print("**********************************************")
+        print('\n')
+
+
+def go_printer():
+    for i in range(len(go_codes)):
+        print("output for", go_paths[i])
+        ruby_list = parser(go_codes[i], go_parser)
+        code = go_codes[i].split('\n')
+        ids = []
+        for e in ruby_list:
+            row = e.start_point[0]
+            col = e.start_point[1]
+            print(code[row][col:e.end_point[1]], "Row Num", row, " Column Num", col)
+        print("**********************************************")
+        print('\n')
+
+
+if language == "python":
+    py_printer()
+elif language == "ruby":
+    ruby_printer()
+elif language == "javascript":
+    js_printer()
+elif language == "go":
+    go_printer()
 
 # for id in py_ids:
 #     for i in id:
 #         if len(id)>
 
-# language=input("enter programming language : ")
-# allowed=['python','ruby','javascript','ruby','go']
-# if language in allowed:
-#  print("allowed")
-#
-# else:
-#     print("language not in allowed list of languages")
-#
+
 # violations=["Capitalisation","Double underscores","Dictionary words","excessive words","enum identifer","external underscores","long id names","naming convention anomally","number of words","numeric identifier","short identifier"]
 #
 # print("Create a file named newfile and newfile2 in the root directory C:/ in windows\n")
